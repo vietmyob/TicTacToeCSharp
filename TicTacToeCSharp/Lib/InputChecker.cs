58df41f205c17bbb44d4f133dae037824965b205
vietanhtran1996 <tranvietanh1996@gmail.com>
2017-11-19 20:02:34 +1300

    Input checker unit test and implementation

---
+++ TicTacToeCSharp/Lib/InputChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToeCSharp.Lib
+{
+    public class InputChecker
+    {
+        public void CheckPosition(string[] board, int userInput)
+        {
+            if(!string.IsNullOrEmpty(board[userInput]))
+                throw new Exception("Selected postion is not empty");
+        }
+    }
+}
---
+++ TicTacToeCSharp/UnitTest/InputCheckerShould.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using TicTacToeCSharp.Lib;
+
+namespace TicTacToeCSharp.UnitTest
+{
+    [TestFixture]
+    public class InputCheckerShould
+    {
+        [Test]
+        public void ReturnErrorIfPositionIsNotEmpty()
+        {
+            var board = new[]
+            {
+                "", "O", "",
+                "", "", "",
+                "", "", ""
+            };
+            var userInput = 1;
+            var inputChecker = new InputChecker();
+            var ex = Assert.Throws<Exception>(() => inputChecker.CheckPosition(board, userInput));
+            Assert.That(ex.Message, Is.EqualTo("Selected postion is not empty"));
+        }
+    }
+}
