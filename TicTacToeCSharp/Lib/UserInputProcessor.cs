ab6a3e455069106b6871012c476c6b2ac706f8cb
vietanhtran1996 <tranvietanh1996@gmail.com>
2017-11-19 21:57:16 +1300

    Refactor

---
+++ TicTacToeCSharp/Lib/UserInputProcessor.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToeCSharp.Lib
+{
+    public class UserInputProcessor
+    {
+        public void Add(string[] board, int index)
+        {
+            board[index] = "O";
+        }
+    }
+}
---
+++ TicTacToeCSharp/UnitTest/UserInputProcessorShould.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TicTacToeCSharp.DTO;
+using TicTacToeCSharp.Lib;
+
+namespace TicTacToeCSharp.UnitTest
+{
+    [TestFixture]
+    public class UserInputProcessorShould
+    {
+        private Board _board;
+        [SetUp]
+        public void SetUp()
+        {
+            _board = new Board();
+        }
+
+        [Test]
+        [TestCase(2)]
+        [TestCase(5)]
+        public void AddUserInput(int index)
+        {
+            var inputProcessor = new UserInputProcessor();
+            inputProcessor.Add(_board.Squares, index);
+            Assert.AreEqual("O", _board.Squares[index]);
+        }
+    }
+}
