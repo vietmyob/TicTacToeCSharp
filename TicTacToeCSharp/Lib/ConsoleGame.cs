872397062edae0b2cad4ff081161678cb9a76dd6
vietanhtran1996 <tranvietanh1996@gmail.com>
2017-11-27 21:54:34 +1300

    Extract Main class

--- TicTacToeCSharp/Lib/ConsoleGame.cs
+++ TicTacToeCSharp/Lib/ConsoleGame.cs
@@ -8,24 +8,29 @@ namespace TicTacToeCSharp.Lib
     {
         static void Main()
         {
-            Play();
+            Execute();
         }
 
-        private static void Play()
+        private static void Execute()
         {
             Console.WriteLine("Let's start the game");
-            var board = new Board(3);
-            const string humanPlayerSymbol = "O";
-            const string computerPlayerSymbol = "X";
-            var computerPlayer = new ComputerPlayer(computerPlayerSymbol);
-            var humanPlayer = new HumanPlayer(new InputChecker(), humanPlayerSymbol);
-            var renderer = new BoardConsoleRenderer();
-            var referee = new WinnerReferee();
+
+            SetUpBoardAndSymbols(out Board board, out string humanPlayerSymbol, out string computerPlayerSymbol);
+            SetUpPlayers(humanPlayerSymbol, computerPlayerSymbol, out ComputerPlayer computerPlayer, out HumanPlayer humanPlayer);
+            SetUpEngine(out BoardConsoleRenderer renderer, out WinnerReferee referee);
 
             computerPlayer.Move(board, 4);
             Console.WriteLine(renderer.Render(board));
-            var winner = string.Empty;
+            var winner = Play(board, humanPlayerSymbol, computerPlayerSymbol, computerPlayer, humanPlayer, renderer, referee);
 
+            AnnounceDrawIfNoWinner(winner);
+
+            AllowReplay();
+        }
+
+        private static string Play(Board board, string humanPlayerSymbol, string computerPlayerSymbol, ComputerPlayer computerPlayer, HumanPlayer humanPlayer, BoardConsoleRenderer renderer, WinnerReferee referee)
+        {
+            var winner = string.Empty;
             while (string.IsNullOrEmpty(winner))
             {
                 Console.WriteLine("Your turn:");
@@ -46,15 +51,42 @@ namespace TicTacToeCSharp.Lib
                 if (board.Squares.All(x => !string.IsNullOrEmpty(x))) break;
             }
 
+            return winner;
+        }
+
+        private static void SetUpEngine(out BoardConsoleRenderer renderer, out WinnerReferee referee)
+        {
+            renderer = new BoardConsoleRenderer();
+            referee = new WinnerReferee();
+        }
+
+        private static void SetUpPlayers(string humanPlayerSymbol, string computerPlayerSymbol, out ComputerPlayer computerPlayer, out HumanPlayer humanPlayer)
+        {
+            computerPlayer = new ComputerPlayer(computerPlayerSymbol);
+            humanPlayer = new HumanPlayer(new InputChecker(), humanPlayerSymbol);
+        }
+
+        private static void SetUpBoardAndSymbols(out Board board, out string humanPlayerSymbol, out string computerPlayerSymbol)
+        {
+            board = new Board(3);
+            humanPlayerSymbol = "O";
+            computerPlayerSymbol = "X";
+        }
+
+        private static void AnnounceDrawIfNoWinner(string winner)
+        {
             if (string.IsNullOrEmpty(winner))
             {
                 Console.WriteLine("It's a draw");
             }
+        }
 
+        private static void AllowReplay()
+        {
             Console.WriteLine("Replay? (Y or N)");
             var reply = Console.ReadLine();
-            if(reply?.ToUpper() == "Y")
-                Play();
+            if (reply?.ToUpper() == "Y")
+                Execute();
 
             Console.ReadKey();
         }
